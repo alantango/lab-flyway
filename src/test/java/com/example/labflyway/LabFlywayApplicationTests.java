bbfc1244c534beaa17dcc7bf2549c9297d2a5162
alantango <alantango@gmail.com>
2022-12-13 23:10:57 -0500

    updates per PR#3

--- src/test/java/com/example/labflyway/LabFlywayApplicationTests.java
+++ src/test/java/com/example/labflyway/LabFlywayApplicationTests.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LabFlywayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 
     @Autowired
